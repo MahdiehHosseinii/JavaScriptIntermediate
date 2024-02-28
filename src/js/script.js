9c8b74d0003882c53962fb3b10d917971957ec50
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-02-28 16:43:37 +0330

    DOM

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// let h1Elem = document.getElementById("title")
+// console.log(h1Elem)
+
+// let liElems = document.getElementsByClassName("list-item")
+// console.log(liElems[2])
+
+// let listElems = document.getElementsByTagName("li")
+// console.log(listElems[3])
+
+// let firstListItem = document.querySelector(".list-item")
+// console.log(firstListItem)
+
+// let ulItems = document.querySelectorAll(".list-item")
+// console.log(ulItems)
+
