20847c9baa1111b8d55cb250043d97516a038f14
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-02-28 19:12:36 +0330

    setAttribute

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,15 @@
 // let ulItems = document.querySelectorAll(".list-item")
 // console.log(ulItems)
 
+// let h1Elem = document.getElementById("title")
+// console.log(h1Elem.getAttribute("align"))
+
+// let h1Elem = document.getElementById("title")
+// h1Elem.setAttribute("class" , "text")
+// console.log(h1Elem)
+
+// let liElems = document.getElementsByClassName("list-item")
+// liElems[1].setAttribute("id" , "js-text")
+// console.log(liElems)
+
+
