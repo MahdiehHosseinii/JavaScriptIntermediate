ceba2534a190e8d034be164cd131a2333ec1c6e2
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-03-15 17:17:22 +0330

    mini project

--- src/js/script.js
+++ src/js/script.js
@@ -317,4 +317,23 @@
 //     console.log("key up")
 // }
 
+let $ = document
+const firstValue = $.querySelector(".C")
+const secondValue = $.querySelector(".F")
+const converter = $.querySelector("#converter")
+const result = $.querySelector(".result")
+const convertButton = $.querySelector(".convertButton")
+const resetButton = $.querySelector(".resetButton")
+const changeButton = $.querySelector(".changeButton")
+function convert () {
 
+}
+function reset () {
+
+}
+function swap () {
+
+}
+convertButton.addEventListener("click" , convert)
+resetButton.addEventListener("click" , reset)
+changeButton.addEventListener("click" , swap)
