bace6c4cb95ccc35b7724c5f41e87b978960bda2
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-02 16:36:43 +0330

    nodes

--- src/js/script.js
+++ src/js/script.js
@@ -417,31 +417,38 @@
 //     console.log(event)
 // })
 
-let $ = document
-let locationKey = $.querySelector("#location")
-let title = $.querySelector("title")
-let keyCodeElem = $.getElementById("keyCode")
-let keyElem = $.getElementById("key")
-let locationElem = $.getElementById("location")
-let whichElem = $.getElementById("which")
-let codeElem = $.getElementById("code")
-document.body.addEventListener("keydown" , function (event){
-    event.preventDefault()
-    starter.style.display="none"
-    heading.style.display="flex"
-    ascii.style.display="flex"
-    infos.style.display="flex"
-    let eventKeyCode = event.keyCode
-    let eventKey = event.key
-    let eventLocation = event.location
-    let eventWhich = event.which
-    let eventCode = event.code
-    keyCodeElem.innerHTML = eventKeyCode
-    keyElem.innerHTML = eventKey
-    locationElem.innerHTML = eventLocation
-    whichElem.innerHTML = eventWhich
-    codeElem.innerHTML = eventCode
-})
+// let $ = document
+// let locationKey = $.querySelector("#location")
+// let title = $.querySelector("title")
+// let keyCodeElem = $.getElementById("keyCode")
+// let keyElem = $.getElementById("key")
+// let locationElem = $.getElementById("location")
+// let whichElem = $.getElementById("which")
+// let codeElem = $.getElementById("code")
+// document.body.addEventListener("keydown" , function (event){
+//     event.preventDefault()
+//     starter.style.display="none"
+//     heading.style.display="flex"
+//     ascii.style.display="flex"
+//     infos.style.display="flex"
+//     let eventKeyCode = event.keyCode
+//     let eventKey = event.key
+//     let eventLocation = event.location
+//     let eventWhich = event.which
+//     let eventCode = event.code
+//     keyCodeElem.innerHTML = eventKeyCode
+//     keyElem.innerHTML = eventKey
+//     locationElem.innerHTML = eventLocation
+//     whichElem.innerHTML = eventWhich
+//     codeElem.innerHTML = eventCode
+// })
+
+let h1Elem = document.getElementById("title")
+console.log(h1Elem.nodeName)
+console.log(h1Elem.nodeType)
+console.log(h1Elem.nodeValue)
+
+
 
 
 
