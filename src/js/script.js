7863555e532eed8e5f80f9a32f26fb772f28c025
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-07 16:26:17 +0330

    onLoad

--- src/js/script.js
+++ src/js/script.js
@@ -563,31 +563,33 @@
 //     console.log(event)
 // }
 
-let contextMenu = document.getElementById("contextMenu")
+// let contextMenu = document.getElementById("contextMenu")
+// function contextHandler(event) {
+//     event.preventDefault()
+//     if (contextMenu.style.display === "none") {
+//         contextMenu.style.left = event.pageX + "px"
+//         contextMenu.style.top = event.pageY + "px"
+//         contextMenu.style.display = "block"
+//     }else {
+//         contextMenu.style.left = event.pageX + "px"
+//         contextMenu.style.top = event.pageY + "px"
+//     }
+// }
+// function hideContextMenu() {
+//     contextMenu.style.display = "none"
+// }
+// function keyDownHandler (event) {
+//     if (event.keyCode === 27) {
+//         contextMenu.style.display = "none"
+//     }
+// }
+// document.body.addEventListener("contextmenu", contextHandler)
+// document.body.addEventListener("click", hideContextMenu)
+// document.body.addEventListener("keydown", keyDownHandler)
 
-function contextHandler(event) {
-    event.preventDefault()
-    if (contextMenu.style.display === "none") {
-        contextMenu.style.left = event.pageX + "px"
-        contextMenu.style.top = event.pageY + "px"
-        contextMenu.style.display = "block"
-    }else {
-        contextMenu.style.left = event.pageX + "px"
-        contextMenu.style.top = event.pageY + "px"
-    }
+function loadHandler() {
+    console.log("loaded")
 }
-function hideContextMenu() {
-    contextMenu.style.display = "none"
-}
-function keyDownHandler (event) {
-    if (event.keyCode === 27) {
-        contextMenu.style.display = "none"
-    }
-}
-
-document.body.addEventListener("contextmenu", contextHandler)
-document.body.addEventListener("click", hideContextMenu)
-document.body.addEventListener("keydown", keyDownHandler)
 
 
 
