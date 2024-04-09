a77cf76781d45fb102649c9b639cc2945311ebfb
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-09 14:33:24 +0330

    mini project

--- src/js/script.js
+++ src/js/script.js
@@ -624,9 +624,19 @@
 //     console.log("selected")
 // }
 
-function touchHandler() {
-    console.log("touched")
-}
+// function touchHandler() {
+//     console.log("touched")
+// }
+
+let $ = document
+
+const inputElem = $.querySelector('input')
+const spanElem = $.querySelector('.counter')
+const inputMaxLength = inputElem.getAttribute('maxlength')
+
+inputElem.addEventListener('keyup', function () {
+    spanElem.innerHTML = inputMaxLength - inputElem.value.length
+})
 
 
 
