6861a97786fddb67f31eed7c2b52b2444050d4dc
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-23 13:13:42 +0330

    sticky navbar

--- src/js/script.js
+++ src/js/script.js
@@ -712,9 +712,23 @@
 // })
 // btnSaveElem.addEventListener("click", generateNewNote)
 
-function scrollHandler(event) {
-    console.log(event)
-}
+// function scrollHandler(event) {
+//     console.log(event)
+// }
+
+const mainNav = document.getElementById("mainNav")
+const logoImg = document.querySelector("img")
+document.addEventListener("scroll" , function () {
+    if (document.documentElement.scrollTop > 0) {
+        mainNav.classList.add("bg-black")
+        mainNav.classList.add("txt-white")
+        logoImg.style.height = "64px"
+    }else {
+        logoImg.style.height = "84px"
+        mainNav.classList.remove("bg-black")
+        mainNav.classList.remove("txt-white")
+    }
+})
 
 
 
