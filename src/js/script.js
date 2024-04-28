43a1c8deb1ff6ee600412e49a4735409df1fe065
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-28 15:02:46 +0330

    prefessional music player

--- src/js/script.js
+++ src/js/script.js
@@ -762,54 +762,210 @@
 //     console.log("current time: " , Math.floor(audioElem.currentTime))
 // }
 
-let audioElem = document.querySelector("audio")
-let musicSrc = [
-    "src/audio/InSeri.mp3",
-    "src/audio/Yadetim.mp3",
-    "src/audio/barMikhore.mp3"
-]
-let audioIndex = 0
+// let audioElem = document.querySelector("audio")
+// let musicSrc = [
+//     "src/audio/InSeri.mp3",
+//     "src/audio/Yadetim.mp3",
+//     "src/audio/barMikhore.mp3"
+// ]
+// let audioIndex = 0
+//
+// function previousMusicHandler() {
+//     audioIndex--
+//     if (audioIndex < 0) {
+//         audioIndex = 2
+//     }
+//     audioElem.setAttribute("src", musicSrc[audioIndex])
+//     playHandler()
+// }
+//
+// function playHandler() {
+//     audioElem.play()
+//     setInterval(function () {
+//         console.log(audioElem.currentTime)
+//     }, 1000)
+// }
+//
+// function pauseHandler() {
+//     audioElem.pause()
+// }
+//
+// function nextMusicHandler() {
+//     audioIndex++
+//     if (audioIndex > musicSrc.length - 1) {
+//         audioIndex = 0
+//     }
+//     audioElem.setAttribute("src", musicSrc[audioIndex])
+//     playHandler()
+// }
+//
+// function speedHandler() {
+//     audioElem.playbackRate = 2
+// }
+//
+// function timeMinusMusicHandler() {
+//     audioElem.currentTime -= 5
+// }
+//
+// function timePlusMusicHandler() {
+//     audioElem.currentTime += 5
+// }
 
-function previousMusicHandler() {
-    audioIndex--
-    if (audioIndex < 0) {
-        audioIndex = 2
+const image = document.querySelector("#cover");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const background = document.getElementById("background");
+
+// Music
+const songs = [
+    {
+        path:
+            "src/audio/InSeri.mp3",
+        displayName: "In Seri",
+        artist: "Shayea & Rez",
+        cover:
+            "src/images/shayea.jpg",
+    },
+    {
+        path: "src/audio/BarMikhore.mp3",
+        displayName: "Yeho Yeja BarMikhore Bet",
+        artist: "Shayea & Tara",
+        cover: "src/images/shayea3.jpg",
+    },
+    {
+        path:
+            "src/audio/Yadetim.mp3",
+        displayName: "Yadetim Koli",
+        artist: "Shayea & Mahyar",
+        cover:
+            "src/images/shayea2.jpg",
+    },
+];
+
+// Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+    music.play();
+}
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+    music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener("click", function () {
+    if (isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
     }
-    audioElem.setAttribute("src", musicSrc[audioIndex])
-    playHandler()
+})
+
+// Update DOM
+function loadSong(song) {
+    console.log(song);
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = song.path;
+    changeCover(song.cover);
 }
 
-function playHandler() {
-    audioElem.play()
-    setInterval(function () {
-        console.log(audioElem.currentTime)
-    }, 1000)
+function changeCover(cover) {
+    image.classList.remove("active");
+    setTimeout(() => {
+        image.src = cover;
+        image.classList.add("active");
+    }, 100);
+    background.src = cover;
 }
 
-function pauseHandler() {
-    audioElem.pause()
-}
+// Current Song
+let songIndex = 0;
 
-function nextMusicHandler() {
-    audioIndex++
-    if (audioIndex > musicSrc.length - 1) {
-        audioIndex = 0
+// Previous Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
     }
-    audioElem.setAttribute("src", musicSrc[audioIndex])
-    playHandler()
+    loadSong(songs[songIndex]);
+    playSong();
 }
 
-function speedHandler() {
-    audioElem.playbackRate = 2
+// Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
 }
 
-function timeMinusMusicHandler() {
-    audioElem.currentTime -= 5
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const duration = e.srcElement.duration;
+        const currentTime = e.srcElement.currentTime;
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = progressPercent + "%";
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = "0" + durationSeconds;
+        }
+        // Delay switching duration Element to avoid NaN
+        if (durationSeconds) {
+            durationEl.textContent = durationMinutes + ":" + durationSeconds;
+        }
+        // Calculate display for currentTime
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = "0" + currentSeconds;
+        }
+        currentTimeEl.textContent = currentMinutes + ":" + currentSeconds;
+    }
 }
 
-function timePlusMusicHandler() {
-    audioElem.currentTime += 5
+// Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = music.duration;
+    music.currentTime = (clickX / width) * duration;
 }
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
+
+
 
 
 
