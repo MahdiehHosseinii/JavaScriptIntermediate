7f9cb2281c5d0f2f384046e49433660bd927423f
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-04-29 13:27:37 +0330

    drag and drop

--- src/js/script.js
+++ src/js/script.js
@@ -982,7 +982,21 @@
 //     })
 // })
 
-console.log(document.querySelector("h1"))
+// console.log(document.querySelector("h1"))
+
+function dragStartHandler(event) {
+    event.dataTransfer.setData("elemId", event.target.id)
+}
+
+function dropHandler(event) {
+    let targetId = event.dataTransfer.getData("elemId")
+    let targetElem = document.getElementById(targetId)
+    event.target.append(targetElem)
+}
+
+function dragOverHandler(event) {
+    event.preventDefault()
+}
 
 
 
