3f368e746cb7821ca087dbfd85a71e42520c7580
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-05-07 14:24:57 +0330

    animation events

--- src/js/script.js
+++ src/js/script.js
@@ -1037,132 +1037,158 @@
 //     }
 // }
 
-let $ = document
-const inputElem = $.getElementById('itemInput')
-const addButton = $.getElementById('addButton')
-const clearButton = $.getElementById('clearButton')
-const todoListElem = $.getElementById('todoList')
+// let $ = document
+// const inputElem = $.getElementById('itemInput')
+// const addButton = $.getElementById('addButton')
+// const clearButton = $.getElementById('clearButton')
+// const todoListElem = $.getElementById('todoList')
+//
+// let todosArray = []
+//
+// function addNewTodo() {
+//     let newTodoTitle = inputElem.value
+//
+//     let newTodoObj = {
+//         id: todosArray.length + 1,
+//         title: newTodoTitle,
+//         complete: false
+//     }
+//
+//     inputElem.value = ''
+//
+//     todosArray.push(newTodoObj)
+//     setLocalStorage(todosArray)
+//     todosGenerator(todosArray)
+//
+//     inputElem.focus()
+// }
+//
+// function setLocalStorage(todosList) {
+//     localStorage.setItem('todos', JSON.stringify(todosList))
+// }
+//
+// function todosGenerator(todosList) {
+//
+//     let newTodoLiElem, newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn
+//
+//     todoListElem.innerHTML = ''
+//
+//     todosList.forEach(function (todo) {
+//         console.log(todo);
+//         newTodoLiElem = $.createElement('li')
+//         newTodoLiElem.className = 'completed well'
+//
+//         newTodoLabalElem = $.createElement('label')
+//         newTodoLabalElem.innerHTML = todo.title
+//
+//         newTodoCompleteBtn = $.createElement('button')
+//         newTodoCompleteBtn.className = 'btn btn-success'
+//         newTodoCompleteBtn.innerHTML = 'Complete'
+//         newTodoCompleteBtn.setAttribute('onclick', 'editTodo(' + todo.id + ')')
+//
+//         newTodoDeleteBtn = $.createElement('button')
+//         newTodoDeleteBtn.className = 'btn btn-danger'
+//         newTodoDeleteBtn.innerHTML = 'Delete'
+//         newTodoDeleteBtn.setAttribute('onclick', 'removeTodo(' + todo.id + ')')
+//
+//         if (todo.complete) {
+//             newTodoLiElem.className = 'uncompleted well'
+//             newTodoCompleteBtn.innerHTML = 'UnComplete'
+//         }
+//
+//         newTodoLiElem.append(newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn)
+//
+//         todoListElem.append(newTodoLiElem)
+//     })
+// }
+//
+// function editTodo(todoId) {
+//
+//     let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+//
+//     todosArray = localStorageTodos
+//
+//     todosArray.forEach(function (todo) {
+//         if (todo.id === todoId) {
+//             todo.complete = !todo.complete
+//         }
+//     })
+//
+//     setLocalStorage(todosArray)
+//     todosGenerator(todosArray)
+// }
+//
+// function removeTodo(todoId) {
+//     let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+//
+//     todosArray = localStorageTodos
+//
+//     let mainTodoIndex = todosArray.findIndex(function (todo) {
+//         return todo.id === todoId
+//     })
+//
+//     todosArray.splice(mainTodoIndex, 1)
+//
+//     setLocalStorage(todosArray)
+//     todosGenerator(todosArray)
+//
+// }
+//
+// function getLocalStorage() {
+//     let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+//
+//     if (localStorageTodos) {
+//         todosArray = localStorageTodos
+//     } else {
+//         todosArray = []
+//     }
+//
+//     todosGenerator(todosArray)
+//
+// }
+//
+// function clearTodos() {
+//     todosArray = []
+//     todosGenerator(todosArray)
+//     localStorage.removeItem('todos')
+// }
+//
+//
+// window.addEventListener('load', getLocalStorage)
+// addButton.addEventListener('click', addNewTodo)
+// clearButton.addEventListener('click', clearTodos)
+// inputElem.addEventListener('keydown', function (event) {
+//     if (event.code === 'Enter') {
+//         addNewTodo()
+//     }
+// })
 
-let todosArray = []
+const addAnimationBtn = document.querySelector("button")
+const divElem = document.querySelector("div")
+const pElem = document.querySelector("p")
 
-function addNewTodo() {
-    let newTodoTitle = inputElem.value
-
-    let newTodoObj = {
-        id: todosArray.length + 1,
-        title: newTodoTitle,
-        complete: false
-    }
-
-    inputElem.value = ''
-
-    todosArray.push(newTodoObj)
-    setLocalStorage(todosArray)
-    todosGenerator(todosArray)
-
-    inputElem.focus()
+function setAnimation () {
+    divElem.style.animation = "move 4s 3"
 }
 
-function setLocalStorage(todosList) {
-    localStorage.setItem('todos', JSON.stringify(todosList))
+function animationStartHandler () {
+    pElem.innerHTML = "Animation Start"
+    divElem.style.backgroundColor = "pink"
+}
+function animationRepeatHandler () {
+    pElem.innerHTML = "Animation Repeat"
+    divElem.style.backgroundColor = "lightblue"
+}
+function animationEndHandler () {
+    pElem.innerHTML = "Animation End"
+    divElem.style.backgroundColor = "gray"
 }
 
-function todosGenerator(todosList) {
+addAnimationBtn.addEventListener("click" , setAnimation)
 
-    let newTodoLiElem, newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn
-
-    todoListElem.innerHTML = ''
-
-    todosList.forEach(function (todo) {
-        console.log(todo);
-        newTodoLiElem = $.createElement('li')
-        newTodoLiElem.className = 'completed well'
-
-        newTodoLabalElem = $.createElement('label')
-        newTodoLabalElem.innerHTML = todo.title
-
-        newTodoCompleteBtn = $.createElement('button')
-        newTodoCompleteBtn.className = 'btn btn-success'
-        newTodoCompleteBtn.innerHTML = 'Complete'
-        newTodoCompleteBtn.setAttribute('onclick', 'editTodo(' + todo.id + ')')
-
-        newTodoDeleteBtn = $.createElement('button')
-        newTodoDeleteBtn.className = 'btn btn-danger'
-        newTodoDeleteBtn.innerHTML = 'Delete'
-        newTodoDeleteBtn.setAttribute('onclick', 'removeTodo(' + todo.id + ')')
-
-        if (todo.complete) {
-            newTodoLiElem.className = 'uncompleted well'
-            newTodoCompleteBtn.innerHTML = 'UnComplete'
-        }
-
-        newTodoLiElem.append(newTodoLabalElem, newTodoCompleteBtn, newTodoDeleteBtn)
-
-        todoListElem.append(newTodoLiElem)
-    })
-}
-
-function editTodo(todoId) {
-
-    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
-
-    todosArray = localStorageTodos
-
-    todosArray.forEach(function (todo) {
-        if (todo.id === todoId) {
-            todo.complete = !todo.complete
-        }
-    })
-
-    setLocalStorage(todosArray)
-    todosGenerator(todosArray)
-}
-
-function removeTodo(todoId) {
-    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
-
-    todosArray = localStorageTodos
-
-    let mainTodoIndex = todosArray.findIndex(function (todo) {
-        return todo.id === todoId
-    })
-
-    todosArray.splice(mainTodoIndex, 1)
-
-    setLocalStorage(todosArray)
-    todosGenerator(todosArray)
-
-}
-
-function getLocalStorage() {
-    let localStorageTodos = JSON.parse(localStorage.getItem('todos'))
-
-    if (localStorageTodos) {
-        todosArray = localStorageTodos
-    } else {
-        todosArray = []
-    }
-
-    todosGenerator(todosArray)
-
-}
-
-function clearTodos() {
-    todosArray = []
-    todosGenerator(todosArray)
-    // localStorage.clear()
-    localStorage.removeItem('todos')
-}
-
-
-window.addEventListener('load', getLocalStorage)
-addButton.addEventListener('click', addNewTodo)
-clearButton.addEventListener('click', clearTodos)
-inputElem.addEventListener('keydown', function (event) {
-    if (event.code === 'Enter') {
-        addNewTodo()
-    }
-})
+divElem.addEventListener("animationstart" , animationStartHandler)
+divElem.addEventListener("animationiteration" , animationRepeatHandler)
+divElem.addEventListener("animationend" , animationEndHandler)
 
 
 
