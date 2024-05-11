53b4d716e610057a5c0257133b04fe64ac637a08
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-05-11 14:32:53 +0330

    BMI

--- src/js/script.js
+++ src/js/script.js
@@ -1237,32 +1237,73 @@
 //     h1Elem.style.cssText = "color: red; background-color:pink; font-size:50px;"
 // }
 
+// let $ = document
+// let sliderImgElem = $.querySelector("img")
+// let previousBtn = $.querySelector(".prev")
+// let nextBtn = $.querySelector(".next")
+// let imgSrcArray = ["src/images/1.jpg", "src/images/2.png", "src/images/3.jpg"]
+// let imgIndex = 0
+//
+// function prevImg() {
+//     imgIndex--
+//     if (imgIndex < 0) {
+//         imgIndex = imgSrcArray.length - 1
+//     }
+//     sliderImgElem.setAttribute("src", imgSrcArray[imgIndex])
+// }
+//
+// function nextImg() {
+//     imgIndex++
+//     if (imgIndex > imgSrcArray.length - 1) {
+//         imgIndex = 0
+//     }
+//     sliderImgElem.setAttribute("src", imgSrcArray[imgIndex])
+// }
+//
+// setInterval(nextImg, 3000)
+// previousBtn.addEventListener("click", prevImg)
+// nextBtn.addEventListener("click", nextImg)
+
 let $ = document
-let sliderImgElem = $.querySelector("img")
-let previousBtn = $.querySelector(".prev")
-let nextBtn = $.querySelector(".next")
-let imgSrcArray = ["src/images/1.jpg", "src/images/2.png", "src/images/3.jpg"]
-let imgIndex = 0
+const weightInput = $.querySelector("#weight")
+const heightInput = $.querySelector("#height")
+const weightValue = $.querySelector("#weight-val")
+const heightValue = $.querySelector("#height-val")
+const bmiResult = $.querySelector("#result")
+const categoryElem = $.querySelector("#category")
 
-function prevImg() {
-    imgIndex--
-    if (imgIndex < 0) {
-        imgIndex = imgSrcArray.length - 1
+function bmiCalculator() {
+    let weightInputValue = weightInput.value
+    let heightInputValue = heightInput.value
+
+    weightValue.innerHTML = weightInputValue + " Kg"
+    heightValue.innerHTML = heightInputValue + " Cm"
+
+    let bmiValue = (weightInputValue / (Math.pow(heightInputValue / 100, 2))).toFixed(1)
+
+    bmiResult.innerHTML = bmiValue
+
+    if (bmiValue < 18.5) {
+        categoryElem.innerHTML = "Under Weight"
+        categoryElem.style.cssText = "color:blue"
+        bmiResult.style.cssText = "color:blue"
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        categoryElem.innerHTML = "Normal Weight :)"
+        categoryElem.style.cssText = "color:green"
+        bmiResult.style.cssText = "color:green"
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+        categoryElem.innerHTML = "Over Weight"
+        categoryElem.style.cssText = "color:yellow"
+        bmiResult.style.cssText = "color:yellow"
+    } else {
+        categoryElem.innerHTML = "Obese"
+        categoryElem.style.cssText = "color:red"
+        bmiResult.style.cssText = "color:red"
     }
-    sliderImgElem.setAttribute("src", imgSrcArray[imgIndex])
 }
 
-function nextImg() {
-    imgIndex++
-    if (imgIndex > imgSrcArray.length - 1) {
-        imgIndex = 0
-    }
-    sliderImgElem.setAttribute("src", imgSrcArray[imgIndex])
-}
-
-setInterval(nextImg, 3000)
-previousBtn.addEventListener("click", prevImg)
-nextBtn.addEventListener("click", nextImg)
+weightInput.addEventListener("input", bmiCalculator)
+heightInput.addEventListener("input", bmiCalculator)
 
 
 
