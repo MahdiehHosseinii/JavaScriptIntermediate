317e11c1780c2413c4dd49eb27026effae8d9a10
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-06-03 14:29:21 +0330

    book list project

--- src/js/script.js
+++ src/js/script.js
@@ -1343,62 +1343,90 @@
 // }
 
 let $ = document
-const addBookBtn = $.querySelector(".add-btn")
-const titleInputElem = $.getElementById("title")
-const authorInputElem = $.getElementById("author")
-const yearInputElem = $.getElementById("year")
-const booksContainer = $.querySelector("#book-list")
+const addBookBtn = $.querySelector('.add-btn')
+const titleInputElem = $.querySelector('#title')
+const authorInputElem = $.querySelector('#author')
+const yearInputElem = $.querySelector('#year')
+const booksContainer = $.querySelector('#book-list')
+
 let books = []
 
-addBookBtn.addEventListener("click", function (event) {
+addBookBtn.addEventListener('click', function (event) {
     event.preventDefault()
 
     let titleInputValue = titleInputElem.value
     let authorInputValue = authorInputElem.value
     let yearInputValue = yearInputElem.value
 
-    let newBookObject = {
-        id: books.length + 1,
-        title: titleInputValue,
-        author: authorInputValue,
-        year: yearInputValue
+
+
+    if (titleInputValue === '' || authorInputValue === '' || yearInputValue === '') {
+        alert('لطفا همه اینپوت ها را پر نمایید')
+    } else {
+        let newBookObject = {
+            id: books.length + 1,
+            title: titleInputValue,
+            author: authorInputValue,
+            year: yearInputValue
+        }
+
+        books.push(newBookObject)
+
+        setIntoLocalStorage(books)
     }
-    books.push(newBookObject)
-    setIntoLocalStorage(books)
+
+
+
 })
 
 function setIntoLocalStorage(allBooksArray) {
-    localStorage.setItem("books", JSON.stringify(allBooksArray))
+    localStorage.setItem('books', JSON.stringify(allBooksArray))
     makeEmptyInputs()
     booksGenerator(allBooksArray)
 }
 
 function makeEmptyInputs() {
-    titleInputElem.value = ""
-    authorInputElem.value = ""
-    yearInputElem.value = ""
+    titleInputElem.value = ''
+    authorInputElem.value = ''
+    yearInputElem.value = ''
 }
 
-function booksGenerator (allBooksArray) {
-    booksContainer.innerHTML = ""
+function booksGenerator(allBooksArray) {
+
+    booksContainer.innerHTML = ''
 
     allBooksArray.forEach(function (book) {
-        let newBookTrElem = $.createElement("tr")
+        newBookTrElem = $.createElement('tr')
 
-        let newBookTitleTh = $.createElement("th")
+        let newBookTitleTh = $.createElement('th')
         newBookTitleTh.innerHTML = book.title
 
-        let newBookAuthorTh = $.createElement("th")
+        let newBookAuthorTh = $.createElement('th')
         newBookAuthorTh.innerHTML = book.author
 
-        let newBookYearTh = $.createElement("th")
+        let newBookYearTh = $.createElement('th')
         newBookYearTh.innerHTML = book.year
 
-        newBookTrElem.append(newBookTitleTh , newBookAuthorTh , newBookYearTh)
+        newBookTrElem.append(newBookTitleTh, newBookAuthorTh, newBookYearTh)
 
         booksContainer.append(newBookTrElem)
+
     })
+
 }
+
+
+function getBooksFromLocalStorage() {
+
+    let localStorageBooks = localStorage.getItem('books')
+
+    if (localStorageBooks) {
+        books = JSON.parse(localStorageBooks)
+        booksGenerator(books)
+    }
+}
+
+window.addEventListener('load', getBooksFromLocalStorage)
 
 
 
