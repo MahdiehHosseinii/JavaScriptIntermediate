71bf09adcec38c5b299f0438127d7e58cc05d680
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-06-14 19:59:44 +0330

    pagination project

--- src/js/script.js
+++ src/js/script.js
@@ -1497,9 +1497,66 @@
 //     }
 // })
 
-window.addEventListener("resize" , function () {
-    console.log("width: " +window.innerWidth + " - height: " + window.innerHeight)
-})
+// window.addEventListener("resize" , function () {
+//     console.log("width: " +window.innerWidth + " - height: " + window.innerHeight)
+// })
+
+const listItems = [
+    {id: 1, name: 'Amin', family: 'Saeedi Rad'},
+    {id: 2, name: 'Amir', family: 'Zehtab'},
+    {id: 3, name: 'Qadir', family: 'Yolme'},
+    {id: 4, name: 'Babak', family: 'Mohammadi'},
+    {id: 5, name: 'Hasan', family: 'Ghahreman Zadeh'},
+
+    {id: 6, name: 'Amin', family: 'Saeedi Rad'},
+    {id: 7, name: 'Amir', family: 'Zehtab'},
+    {id: 8, name: 'Qadir', family: 'Yolme'},
+    {id: 9, name: 'Babak', family: 'Mohammadi'},
+    {id: 10, name: 'Hasan', family: 'Ghahreman Zadeh'},
+
+    {id: 11, name: 'Saeed', family: 'Ehsani'},
+    {id: 12, name: 'Siamak', family: 'Modiri'},
+    {id: 13, name: 'Mohsen', family: 'Ansari'},
+    {id: 14, name: 'Mehran', family: 'Ali Pour'},
+    {id: 15, name: 'Amir Hossein', family: 'Mahtabi'},
+
+    {id: 16, name: 'Hossein', family: 'Amino'},
+    {id: 17, name: 'Melika', family: 'Ehsani'},
+    {id: 18, name: 'Qadir', family: 'Yolme'},
+    {id: 19, name: 'Fatemeh', family: 'Alilou'},
+    {id: 20, name: 'Ehsan', family: 'Tayyebi'},
+
+    {id: 21, name: 'Zahra', family: 'Gholami'},
+    {id: 22, name: 'Matin', family: 'Sahebi'},
+
+];
+
+let userListContainer = document.querySelector("#list")
+let paginationContainer = document.querySelector("#pagination")
+
+let currentPage = 1
+let rowsCount = 5
+
+function displayUsersList(allUsersArray, usersContainer, rowsCount, currentPage) {
+    usersContainer.innerHTML = ""
+
+    let endIndex = rowsCount * currentPage
+    let startIndex = endIndex - rowsCount
+
+    let paginatedUsers = allUsersArray.slice(startIndex, endIndex)
+
+    paginatedUsers.forEach(function (user) {
+        let userElement = document.createElement("div")
+        userElement.classList.add("item")
+        userElement.innerHTML = user.name + " " + user.family
+
+        usersContainer.appendChild(userElement)
+    })
+
+    console.log(paginatedUsers)
+}
+
+displayUsersList(listItems, userListContainer, rowsCount, currentPage)
 
 
 
