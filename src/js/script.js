a7041b3c11f034cef055369fffb368f41c3ec23c
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-07-08 13:30:46 +0330

    reload method

--- src/js/script.js
+++ src/js/script.js
@@ -2161,16 +2161,22 @@
 //     userInfo.innerHTML = "user is not defined"
 // }
 
-let productsContainer = document.querySelector('.container')
+// let productsContainer = document.querySelector('.container')
+//
+// let productsArray = [
+//     { id: 1, title: 'Sport Shoe', price: 53, img: 'src/images/1.png' },
+//     { id: 2, title: 'Women Shoe', price: 81, img: 'src/images/2.png' },
+//     { id: 3, title: 'Boots', price: 34, img: 'src/images/3.png' }
+// ]
+//
+// productsArray.forEach(function (product) {
+//     productsContainer.insertAdjacentHTML('beforeend', '<div class="product-card"><h1>' + product.title + '</h1><p>Lorem ipsum, or lipsum as it is sometimes known</p><div class="product-pic" style="background-image: url(' + product.img + ');"></div><div class="product-info"><div class="product-price">$' + product.price + '</div><a href="product.html?id=' + product.id + '" class="product-button">See More</a></div></div>')
+// })
 
-let productsArray = [
-    { id: 1, title: 'Sport Shoe', price: 53, img: 'src/images/1.png' },
-    { id: 2, title: 'Women Shoe', price: 81, img: 'src/images/2.png' },
-    { id: 3, title: 'Boots', price: 34, img: 'src/images/3.png' }
-]
+let reloadBtn = document.querySelector("button")
 
-productsArray.forEach(function (product) {
-    productsContainer.insertAdjacentHTML('beforeend', '<div class="product-card"><h1>' + product.title + '</h1><p>Lorem ipsum, or lipsum as it is sometimes known</p><div class="product-pic" style="background-image: url(' + product.img + ');"></div><div class="product-info"><div class="product-price">$' + product.price + '</div><a href="product.html?id=' + product.id + '" class="product-button">See More</a></div></div>')
+reloadBtn.addEventListener("click" , function () {
+    location.reload()
 })
 
 
